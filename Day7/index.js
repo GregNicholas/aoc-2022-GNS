5de852a73231625ad3b4a49231f50f63935a6f6d
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-12-06 12:09:54 -0800

    setup day 7

---
+++ Day7/index.js
@@ -0,0 +1,15 @@
+// import { format } from "path";
+const fs = require("fs");
+
+// ***** day 7 part 1 https://adventofcode.com/2022/day/7 *****
+const data = fs.readFileSync("/data/day6.txt", { encoding: "utf-8" });
+console.log(data);
+
+const partOne = (data) => {};
+
+// console.log(partOne(data));
+// *** part 2
+
+const partTwo = (data) => {};
+
+// console.log(partTwo(data));
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # aoc-2022-GNS
-Created with CodeSandbox
+
+javascript
+// examples of pulling data from input page
+document.documentElement.textContent.trim('\n')
+document.documentElement.textContent.trim().split('\n')
