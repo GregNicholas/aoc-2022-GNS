93bbc5e74a2bec1813cfe4056992878d5d259847
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-12-07 19:17:46 -0800

    Day 7

--- Day7/index.js
+++ Day7/index.js
@@ -2,14 +2,51 @@
 const fs = require("fs");
 
 // ***** day 7 part 1 https://adventofcode.com/2022/day/7 *****
-const data = fs.readFileSync("/data/day6.txt", { encoding: "utf-8" });
-console.log(data);
+const commands = fs
+  .readFileSync("/data/day7.txt", { encoding: "utf-8" })
+  .split("\n");
 
-const partOne = (data) => {};
+const TOTAL_SIZE = 70000000;
+const NEED_FREE = 30000000;
 
-// console.log(partOne(data));
-// *** part 2
+console.log(commands);
+const partOne = (commands) => {
+  const dirs = { "/": 0 };
+  const dirStack = ["/"];
 
-const partTwo = (data) => {};
+  for (let i = 1; i < commands.length; i++) {
+    const cmd = commands[i];
+    if (cmd.startsWith("$ cd ..")) {
+      dirStack.pop();
+    } else if (cmd.startsWith("$ cd")) {
+      const curDirectory = dirStack.toString() + cmd.split(" ")[2];
+      if (dirs[curDirectory] === undefined) {
+        dirs[curDirectory] = 0;
+      }
+      dirStack.push(curDirectory);
+    } else if (cmd.startsWith("$ ls") || cmd.startsWith("dir")) {
+      continue;
+    } else {
+      dirStack.forEach((dir) => {
+        dirs[dir] += parseInt(cmd.split(" ")[0]);
+      });
+    }
+  }
 
-// console.log(partTwo(data));
+  const needToDelete = NEED_FREE - (TOTAL_SIZE - dirs["/"]);
+  const willDelete = Object.values(dirs)
+    .filter((val) => val >= needToDelete)
+    .sort((a, b) => a - b)[0];
+
+  console.log("part 2: ", willDelete);
+
+  let sum = 0;
+  Object.keys(dirs).forEach((dir) => {
+    if (dirs[dir] <= 100000) {
+      sum += dirs[dir];
+    }
+  });
+  return sum;
+};
+
+console.log("Part 1: ", partOne(commands));
