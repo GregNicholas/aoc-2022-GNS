f54a9c21b78485deeea3d03ed7ee664eac6c02ae
GregNicholas <59461870+GregNicholas@users.noreply.github.com>
2022-12-09 15:36:35 -0800

    setup day 10

---
+++ Day10/index.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// ***** day 8 part 1 https://adventofcode.com/2022/day/8 *****
+const data = fs
+  .readFileSync("/data/day10test.txt", { encoding: "utf-8" })
+  .split("\n");
+
+const partOne = () => {};
+
+const partTwo = () => {};
